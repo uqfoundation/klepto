34daada7a165d81de26fbed9a804af6f7dd4b6b3
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2014-01-21 19:53:39 +0000

    updated cache_info test, apparently results for archiving slightly changed
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/klepto@584 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- tests/test_cache_info.py
+++ tests/test_cache_info.py
@@ -21,11 +21,13 @@ def _test_hits(algorithm, maxsize=20, keymap=None,
         r = f(choice(domain), choice(domain))
 
     f.dump()
+   #print(f.info())
     return f.info()
 
 
 if __name__ == '__main__':
 
+    import os 
     import sys
     PY32 = ( hex(sys.hexversion) >= '0x30200f0' )
     from klepto import *
@@ -33,6 +35,9 @@ if __name__ == '__main__':
     seed(1234) # random seed
 
     caches = [rr_cache,mru_cache,lru_cache,lfu_cache,inf_cache,no_cache]
+
+    # clean-up
+    if os.path.exists('cache.pkl'): os.remove('cache.pkl')
 
    #print ("WITHOUT ARCHIVE")
     results = [_test_hits(cache, maxsize=100,
@@ -73,34 +78,37 @@ if __name__ == '__main__':
    #print ("\nWITH ARCHIVE")
     results = [_test_hits(cache, maxsize=100, rangelimit=20,
                           tries=100, archived=True) for cache in caches]
+    # clean-up
+    if os.path.exists('cache.pkl'): os.remove('cache.pkl')
+
     x = results[0]
     if PY32:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (12,88,0,100,88)
-    else:
         assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,89,0,100,89)
+    else:
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (10,90,0,100,90)
     x = results[1]
     if PY32:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (8,68,24,100,92)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,67,22,100,89)
     else:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (12,70,18,100,88)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,68,21,100,89)
     x = results[2]
     if PY32:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,58,31,100,89)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (10,57,33,100,90)
     else:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (9,54,37,100,91)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (9,51,40,100,91)
     x = results[3]
     if PY32:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,36,53,100,89)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (15,42,43,100,85)
     else:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,42,47,100,89)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,45,44,100,89)
     x = results[4]
     if PY32:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (5,37,58,None,95)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,29,60,None,89)
     else:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (11,31,58,None,89)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (13,30,57,None,87)
     x = results[5]
     if PY32:
-        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (0,18,82,0,0)
+        assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (0,23,77,0,0)
     else:
         assert (x.hit, x.miss, x.load, x.maxsize, x.size) == (0,22,78,0,0)
    #for cache in caches:
@@ -108,9 +116,6 @@ if __name__ == '__main__':
    #    msg += "%s" % str(_test_hits(cache, maxsize=100,
    #                                 rangelimit=20, tries=100, archived=True))
    #    print (msg)
-
-    import os 
-    os.remove('cache.pkl')
 
 
 # EOF
