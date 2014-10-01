b4e02d65935c86ada118e57671c79b1efe778d85
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2014-10-01 21:13:32 +0000

    enable test_cachekeys to iterate over archive types
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/klepto@707 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- tests/test_cachekeys.py
+++ tests/test_cachekeys.py
@@ -2,49 +2,112 @@ from klepto import inf_cache as memoized
 from klepto.archives import *
 from klepto.keymaps import picklemap
 
-pm = picklemap(serializer='dill')
-fname = 'xxxxxx'
-delete = False
+try:
+    import ___________ #XXX: enable test w/o numpy.arrays
+    import numpy as np
+    data = np.arange(20)
+except ImportError:
+    data = range(20)
 
-### OK
-#ar = file_archive(fname, serialized=True, cached=False)
-#ar = file_archive(fname, serialized=True, cached=True)
-#ar = null_archive(fname, serialized=True, cached=False)
-#ar = null_archive(fname, serialized=True, cached=True)
-#ar = dict_archive(fname, serialized=True, cached=False)
-#ar = dict_archive(fname, serialized=True, cached=True)
-#ar = dir_archive(fname, serialized=True, cached=False)
-ar = dir_archive(fname, serialized=True, cached=True)
-### throws RecursionError   NOTE: '\x80' not valid, but r'\x80' is valid
-#ar = sql_archive(fname, serialized=True, cached=False)
-#ar = sql_archive(fname, serialized=True, cached=True)
-### throws sql ProgrammingError (warns to use unicode strings)
-#ar = sqltable_archive(fname, serialized=True, cached=False)
-#ar = sqltable_archive(fname, serialized=True, cached=True)
-
-@memoized(cache=ar, keymap=pm)
-def testit(x):
-  return x
-
-testit(1)
-testit(2)
-# testit(numpy.array([1,2]))
-
-testit.load()
-testit.dump()
-c = testit.__cache__()
-print(c)
-print(getattr(c, '__archive__', ''))
-
-print(testit.info())
-
-if delete:
+def remove(name):
     try:
         import os
-        os.remove(fname)
+        os.remove(name)
     except: #FileNotFoundError
         import pox
-        pox.shutils.rmtree(fname, self=True, ignore_errors=True)
+        pox.shutils.rmtree(name, self=True, ignore_errors=True)
+    return
+
+archives = [
+### OK
+  null_archive,
+  dict_archive,
+  file_archive,
+### FIXME: on numpy array, throws ValueError('I/O operation on closed file')
+  dir_archive,
+### FIXME: throws RecursionError  NOTE: '\x80' not valid, but r'\x80' is valid
+# sql_archive,
+### FIXME: throws sql ProgrammingError (warns to use unicode strings)
+# sqltable_archiver,
+]
+
+
+def runme(arxiv, expected=None):
+
+    pm = picklemap(serializer='dill')
+
+    @memoized(cache=arxiv, keymap=pm)
+    def testit(x):
+        return x
+
+    testit(1)
+    testit('2')
+    testit(data)
+    testit(lambda x:x**2)
+
+    testit.load()
+    testit.dump()
+    c = testit.__cache__()
+    r = getattr(c, '__archive__', '')
+    info = testit.info()
+    ck = c.keys()
+    rk = r.keys() if r else ck
+#   print(type(c))
+#   print(c)
+#   print(r)
+#   print(info)
+
+    # check keys are identical in cache and archive
+    assert sorted(ck) == sorted(rk)
+
+    xx = len(ck) or max(info.hit, info.miss, info.load)
+
+    # check size and behavior
+    if expected == 'hit':
+        assert (info.hit, info.miss, info.load) == (xx, 0, 0)
+    elif expected == 'load':
+        assert (info.hit, info.miss, info.load) == (0, 0, xx)
+    else:
+        assert (info.hit, info.miss, info.load) == (0, xx, 0)
+    return
+
+def test_cache(archive, name, delete=True):
+
+    arname = 'xxxxxx'+ str(name)
+    acname = 'xxxyyy'+ str(name)
+
+    import os
+    rerun = 'hit'
+    hit = 'hit' if os.path.exists(arname) else None
+    load = 'load' if os.path.exists(acname) else None
+
+    # special cases
+    if archive == null_archive:
+        rerun, hit, load = None, None, None
+    elif archive == dict_archive:
+        hit, load = None, None
+
+    ar = archive(arname, serialized=True, cached=False)
+    runme(ar, hit)
+    runme(ar, rerun)
+    if delete:
+        remove(arname)
+    ac = archive(acname, serialized=True, cached=True)
+    runme(ac, load)
+    runme(ac, 'hit')
+    if delete:
+        remove(acname)
+    return
+
+count = 0
+for archive in archives:
+    test_cache(archive, count, delete=False)
+    count += 1
+
+count = 0
+for archive in archives:
+    test_cache(archive, count, delete=True)
+    count += 1
 
 
 # EOF
