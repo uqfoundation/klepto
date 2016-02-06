3f6bcb0288253b7bb1ca79f0dc3ef27b85c1aa73
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2016-02-06 16:28:12 +0000

    nudging support to dill-0.2.5
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/klepto@851 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ Requirements
 Klepto requires::
 
     - python2, version >= 2.5  *or*  python3, version >= 3.1
-    - dill, version >= 0.2.4
+    - dill, version >= 0.2.5
     - pox, version >= 0.2.2
 
 Optional requirements::
@@ -251,7 +251,7 @@ setup(name='klepto',
 """ % (target_version, long_description)
 
 # add dependencies
-dill_version = '>=0.2.4'
+dill_version = '>=0.2.5'
 pox_version = '>=0.2.2'
 sqlalchemy_version = '>=0.8.4'
 import sys
