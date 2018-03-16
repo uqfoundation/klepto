27a565f062d357497c412066c54df911914cd1e7
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2018-03-16 22:16:57 +0000

    tweak tests where str of dicts might be ordered differently
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/klepto@1025 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- tests/test_keymaps.py
+++ tests/test_keymaps.py
@@ -7,7 +7,7 @@
 #  - https://github.com/uqfoundation/klepto/blob/master/LICENSE
 
 from klepto.keymaps import *
-from dill import dumps
+from dill import dumps, loads
 import sys
 is342 = sys.version_info[:3] == (3,4,2)
 
@@ -38,22 +38,24 @@ def test_stringmap():
     assert encode(*args, **kwds) == "(1, 2, 'a', 3, 'b', 4)"
     if not is342:
         encode = stringmap(typed=False, flat=False, sentinel=NOSENTINEL)
-        assert encode(*args, **kwds) == str((args, kwds))
+        assert eval(encode(*args, **kwds)) == (args, kwds)
+        #res = encode(*args, **kwds)
+        #assert res in ("({}, {})".format(str(args),_kwds), "({}, {})".format(str(args),kwds_))
     encode = stringmap(typed=True, flat=True, sentinel=NOSENTINEL)
     assert encode(*args, **kwds) == str( (1, 2, 'a', 3, 'b', 4, type(1), type(2), type(3), type(4)) )
     if not is342:
         encode = stringmap(typed=True, flat=False, sentinel=NOSENTINEL)
-        assert encode(*args, **kwds) == str( (args, kwds, (type(1), type(2)), (type(3), type(4))) )
+        assert eval(encode(*args, **kwds).replace(str((type(1), type(2))), "''")) == (args, kwds, '', '')
 
 def test_picklemap():
     encode = picklemap(typed=False, flat=True, serializer='dill')
     assert encode(*args, **kwds) == dumps((1, 2, 'a', 3, 'b', 4))
     encode = picklemap(typed=False, flat=False, serializer='dill')
-    assert encode(*args, **kwds) == dumps((args, kwds))
+    assert loads(encode(*args, **kwds)) == loads(dumps((args, kwds)))
     encode = picklemap(typed=True, flat=True, serializer='dill')
     assert encode(*args, **kwds) == dumps( (1, 2, 'a', 3, 'b', 4, type(1), type(2), type(3), type(4)) )
     encode = picklemap(typed=True, flat=False, serializer='dill')
-    assert encode(*args, **kwds) == dumps( (args, kwds, (type(1), type(2)), (type(3), type(4))) )
+    assert loads(encode(*args, **kwds)) == loads(dumps( (args, kwds, (type(1), type(2)), (type(3), type(4))) ))
 
 
 if __name__ == '__main__':
