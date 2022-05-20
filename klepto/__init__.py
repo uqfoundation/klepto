3c5795c915f6e6bdb40de34de385216014a5c29e
mmckerns <mmckerns@caltech.edu>
2022-05-20 06:04:31 -0400

    remove wheel requirement

--- klepto/__init__.py
+++ klepto/__init__.py
@@ -121,7 +121,6 @@ Requirements
 
     - ``python`` (or ``pypy``), **==2.7** or **>=3.7**
     - ``setuptools``, **>=42**
-    - ``wheel``, **>=0.1**
     - ``dill``, **>=0.3.5**
     - ``pox``, **>=0.3.1**
 
